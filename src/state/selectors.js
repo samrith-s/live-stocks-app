02ba0b27d68b1870093a8b7c8d9ec984cb9cc168
Samrith Shankar <samrith@ithaka.travel>
2017-09-02 11:32:37 +0530

    Any new key added to data now directly is passed by selector

--- src/state/selectors.js
+++ src/state/selectors.js
@@ -1,3 +1,18 @@
+import _ from 'lodash';
+
 export const getData = state => {
-  return state.data || {}
+  let data = state.app.data;
+  let construedData = null;
+
+  if(!_.isEmpty(data)) {
+    construedData = Object.keys(data).map((item,index) => {
+      let thisItem = data[item];
+      return {
+        name: item,
+        ...thisItem
+      }
+    });
+
+    return construedData;
+  }
 }
