9f427aad97f487720d95fd66536038c602894cee
Samrith Shankar <samrith@ithaka.travel>
2017-09-02 11:32:06 +0530

    Integrated stockitem in appwrapper

--- src/components/AppWrapper.jsx
+++ src/components/AppWrapper.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import _ from 'lodash';
+
+import StockItem from './StockItem';
 
 export default class AppWrapper extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      orderBy: 'name'
+    }
+  }
+
   componentDidMount() {
     this.props.actions.startListeningToData();
+  }
+
+  renderStocksList = (stocks) => {
+    if(stocks)
+      return stocks.map((stock, index) => {
+        return (
+          <StockItem data={stock} index={index} key={`stock-item-${stock.name}`} />
+        )
+      });
   }
 
   render() {
     return (
       <div className="app">
-        APP!
+        <div className="stock-list-container">
+          <div className="left-half">
+            {this.renderStocksList(_.orderBy(this.props.data,[this.state.orderBy],['asc']))}
+          </div>
+          <div className="right-half">
+            some logo
+          </div>
+        </div>
       </div>
     )
   }
