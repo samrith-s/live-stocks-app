c86043d4c6df3d07b09694e5b2ee8eb47b1b3d1c
Samrith Shankar <samrith@ithaka.travel>
2017-09-03 13:26:24 +0530

    Solved scrolling issue by maintaining scroll  state

--- src/components/AppWrapper.jsx
+++ src/components/AppWrapper.jsx
@@ -2,37 +2,45 @@ import React from 'react';
 import _ from 'lodash';
 
 import StockItem from './StockItem';
-import Scrollbars from 'react-custom-scrollbars';
+import Insights from './Insights';
 
 export default class AppWrapper extends React.Component {
   constructor() {
     super();
     this.state = {
       sortBy: 'price',
-      orderBy: 'asc'
+      orderBy: 'asc',
+      topScroll: 0
     }
   }
 
   componentDidMount() {
     this.props.actions.startListeningToData();
+    this.scroller.onscroll = () => { this.setState(_.merge({}, this.state, { topScroll: this.scroller.scrollTop })) }
   }
 
   renderStocksList = (stocks, stockactions) => {
     if(stocks)
-      return stocks.filter(stock => !stock.pinned).map((stock, index) => {
+      return stocks.map((stock, index) => {
         return (
           <StockItem data={stock} index={index} key={`stock-item-${stock.name}`} actions={stockactions} />
         )
       });
   }
 
+  componentDidUpdate() {
+    this.scroller.scrollTop = this.state.topScroll;
+  }
+
   renderPinnedStocks = (stocks, stockactions) => {
-    if(stocks)
-      return stocks.filter(stock => stock.pinned).map((stock,index) => {
+    if(stocks && stocks.length>0)
+      return stocks.map((stock,index) => {
         return (
           <StockItem data={stock} index={index} key={`stock-item-pinned-${stock.name}`} actions={stockactions} />
         )
       })
+    else
+      return <div className="no-pinned">No pinned stocks! Click on a stock to pin it.</div>
   }
 
   getClass = (string,action) => {
@@ -48,17 +56,26 @@ export default class AppWrapper extends React.Component {
   }
 
   render() {
+    let stocks = this.props.data || [];
     let stockactions = _.pick(this.props.actions || {}, ['pinStock', 'unpinStock']);
+
     return (
       <div className="app">
-          <Scrollbars
-            renderView={props => <div className="inner-scroller" {...props} />}
-          >
+          <div className="inner-scroller" ref={ref => this.scroller = ref}>
+            <h3 className="logo inner">
+              <i className="logo" />Live Stock App
+            </h3>
+
+            <Insights min={this.props.min || {}} max={this.props.max || {}} />
+
             <div className="pinned-stocks inner">
-              <div className="list-header">
-                <h3><i className="fa fa-thumb-tack" /> Pinned Stocks</h3>
+              <div className="pinner-stocks-wrapper">
+                <div className="list-header not-inline">
+                  <h3><i className="fa fa-thumb-tack" /> Pinned Stocks</h3>
+                  <p>Pinned stocks are automatically sorted by name. Click on any pinned stock to unpin.</p>
+                </div>
+                {this.renderPinnedStocks(_.orderBy(stocks.filter(stock => stock.pinned), ['name'], ['asc']), stockactions)}
               </div>
-              {this.renderPinnedStocks(_.orderBy(this.props.data, ['name'], ['asc']), stockactions)}
             </div>
 
             <div className="stock-list-container inner">
@@ -75,9 +92,10 @@ export default class AppWrapper extends React.Component {
                   </div>
                 </div>
               </div>
-              {this.renderStocksList(_.orderBy(this.props.data,[this.state.sortBy],[this.state.orderBy]), stockactions)}
+
+              {this.renderStocksList(_.orderBy(stocks.filter(stock => !stock.pinned),[this.state.sortBy],[this.state.orderBy]), stockactions)}
             </div>
-        </Scrollbars>
+        </div>
       </div>
     )
   }
