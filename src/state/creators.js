e4a19d74df7a61ce0325228fa495f35c8cb28506
Samrith Shankar <samrith@ithaka.travel>
2017-09-04 03:12:22 +0530

    removed console.log

--- src/state/creators.js
+++ src/state/creators.js
@@ -2,7 +2,7 @@ import Actions from './actions';
 
 const startListeningToData = () => { return { type: Actions.START_LISTENING }}
 
-const messageReceiver = (message) => { console.log('message-received'); return { type: Actions.MESSAGE_RECEIVED, message: message }}
+const messageReceiver = (message) => { return { type: Actions.MESSAGE_RECEIVED, message: message }}
 
 const pinStock = (stock) => { return { type: Actions.PIN_STOCK, stock }}
 const unpinStock = (stock) => { return { type: Actions.UNPIN_STOCK, stock }}
