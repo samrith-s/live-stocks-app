2a4175475baa2d63455ff9260fd07f260bf36dd3
Samrith Shankar <samrith@ithaka.travel>
2017-09-03 13:45:50 +0530

    WIP on insights data selector

--- src/state/selectors.js
+++ src/state/selectors.js
@@ -21,7 +21,7 @@ export const getData = state => {
 export const getMax = createSelector(
   getData,
   (data) => {
-    let maxObj = _.max(data, _.property('history'));
+    let maxObj = _.max(data, _.property('history')) || {};
     return {
       name: maxObj.name,
       price: _.max(maxObj.history)
@@ -32,7 +32,7 @@ export const getMax = createSelector(
 export const getMin = createSelector(
   getData,
   (data) => {
-    let minObj = _.min(data, _.property('history'));
+    let minObj = _.min(data, (item) => _.min(item, _.property('history'))) || {};
     return {
       name: minObj.name,
       price: _.min(minObj.history)
